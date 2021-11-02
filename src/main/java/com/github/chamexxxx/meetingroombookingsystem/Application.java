5add2ff321345db62c8c871073d349cf03b6237b
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-03 01:34:40 +0400

    change scene width and height

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/Application.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.getIcons().add(new Image("file:logo.png"));
         stage.setTitle("Meeting room booking system");
         stage.setScene(scene);
