03638888350980ae9d5d9f7ecb2ab6a037348cce
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-03 00:14:04 +0400

    replace hello controller with login controller

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/Application.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/Application.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 240);
         stage.setTitle("Meeting room booking system");
         stage.setScene(scene);
---
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/LoginController.java
@@ -0,0 +1,13 @@
+package com.github.chamexxxx.meetingroombookingsystem;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+}
