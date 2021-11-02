bf14b2bf33ead66be33f71853c0649ccaba545c1
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-03 02:41:53 +0400

    add user data validation

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/LoginController.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/LoginController.java
@@ -1,6 +1,7 @@
 package com.github.chamexxxx.meetingroombookingsystem;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -10,4 +11,20 @@ public class LoginController {
 
     @FXML
     private PasswordField password;
+
+    @FXML
+    protected void onLoginButtonClick() throws Exception {
+        if (username.getText().equals("username") && password.getText().equals("password")) {
+            // redirect to home scene
+        } else {
+            showLoginError();
+        }
+    }
+
+    private void showLoginError() {
+        var errorAlert = new Alert(Alert.AlertType.ERROR);
+        errorAlert.setHeaderText(null);
+        errorAlert.setContentText("Wrong login or password");
+        errorAlert.showAndWait();
+    }
 }
