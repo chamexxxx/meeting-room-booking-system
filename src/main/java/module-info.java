d7b32a5b2a9557cf41188d30b62facaae475528c
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-10 18:35:09 +0400

    add calendarfx dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.github.chamexxxx.meetingroombookingsystem {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires com.calendarfx.view;
 
     opens com.github.chamexxxx.meetingroombookingsystem to javafx.fxml;
     exports com.github.chamexxxx.meetingroombookingsystem;
