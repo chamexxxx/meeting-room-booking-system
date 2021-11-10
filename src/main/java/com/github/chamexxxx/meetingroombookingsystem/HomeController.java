e3c920ec893273336ddae065e4ecbdb49e57f9fa
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-10 18:58:08 +0400

    replace custom table with calendarfx dependency

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/HomeController.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/HomeController.java
@@ -1,30 +1,27 @@
 package com.github.chamexxxx.meetingroombookingsystem;
 
+import com.calendarfx.model.*;
+import com.calendarfx.view.page.WeekPage;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.*;
 
 public class HomeController implements Initializable {
     @FXML
-    private TableView<Meet> table;
-
-    private final String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    private WeekPage weekPage;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ObservableList<Meet> meets = FXCollections.observableArrayList();
 
-        this.configureTable(table, meets);
+        configureWeekPage();
+    }
 
-        var columns = getTableColumns();
-
-        table.getColumns().addAll(columns);
+    private void configureWeekPage() {
     }
 
     private ArrayList<TableColumn<Meet, String>> getTableColumns() {
