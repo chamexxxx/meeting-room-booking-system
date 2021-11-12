34e343b2b86a8d7de4b612088c96ae980bfbef0b
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-12 15:51:45 +0400

    add creating database tables

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/Application.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/Application.java
@@ -1,21 +1,29 @@
 package com.github.chamexxxx.meetingroombookingsystem;
 
+import com.github.chamexxxx.meetingroombookingsystem.models.Account;
+import com.github.chamexxxx.meetingroombookingsystem.models.Meet;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Locale;
 
 public class Application extends javafx.application.Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, SQLException {
         Locale.setDefault(Locale.US);
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
 
         configureStage(stage);
+
+        configureDatabase();
 
         stage.setScene(scene);
         stage.show();
@@ -27,6 +35,16 @@ public class Application extends javafx.application.Application {
         stage.setMinWidth(800);
         stage.setMinHeight(600);
         stage.setMaximized(true);
+    }
+
+    private void configureDatabase() throws SQLException {
+        ConnectionSource connectionSource = new JdbcConnectionSource("jdbc:sqlite:database.db");
+        createDatabaseTables(connectionSource);
+    }
+
+    private void createDatabaseTables(ConnectionSource connectionSource) throws SQLException {
+        TableUtils.createTableIfNotExists(connectionSource, Account.class);
+        TableUtils.createTableIfNotExists(connectionSource, Meet.class);
     }
 
     public static void main(String[] args) {
