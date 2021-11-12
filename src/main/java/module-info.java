86955f06bc6cc887b758f967087243715c6ca8c9
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-12 15:36:51 +0400

    add ormlite dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.github.chamexxxx.meetingroombookingsystem {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.calendarfx.view;
+    requires java.sql;
+    requires ormlite.core;
+    requires ormlite.jdbc;
 
     opens com.github.chamexxxx.meetingroombookingsystem to javafx.fxml;
     exports com.github.chamexxxx.meetingroombookingsystem;
