e25515fc74ec4ec88d63e92a091ac250678f074d
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-15 10:28:25 +0400

    remove duplicate methods in home controller

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/HomeController.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/HomeController.java
@@ -136,31 +136,4 @@ public class HomeController implements Initializable {
     private List<Meet> getMeets() throws SQLException {
         return Database.meetDao.queryForAll();
     }
-
-    private void configureWeekPage(WeekPage weekPage) {
-        weekPage.getCalendarSources().add(meetCalendarSource);
-
-        for (String day : days) {
-            var column = new TableColumn<Meet, String>(day);
-
-            this.configureColumn(column, 50);
-
-            columns.add(column);
-        }
-
-        return columns;
-    }
-
-    private void configureTable(TableView<Meet> table, ObservableList<Meet> items) {
-        table.setItems(items);
-        table.setSelectionModel(null);
-        table.setFocusTraversable(false);
-        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-    }
-
-    private void configureColumn(TableColumn<?,?> column, Integer size) {
-        column.setReorderable(false);
-        column.setSortable(false);
-        column.setMaxWidth(1f * Integer.MAX_VALUE * size);
-    }
 }
