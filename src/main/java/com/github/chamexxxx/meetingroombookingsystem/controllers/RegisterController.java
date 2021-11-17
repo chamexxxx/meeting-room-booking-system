38f9e72a701ad4227ecbd9d4a55711bac5256316
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-17 18:54:25 +0400

    add validation register form

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/controllers/RegisterController.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/controllers/RegisterController.java
@@ -3,6 +3,7 @@ package com.github.chamexxxx.meetingroombookingsystem.controllers;
 import com.github.chamexxxx.meetingroombookingsystem.control.RegisterForm;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 
 import java.net.URL;
 import java.util.HashMap;
@@ -18,5 +19,43 @@ public class RegisterController implements Initializable {
     }
 
     private void onRegisterButtonClick(HashMap<String, String> formData) {
+        var username = formData.get("username");
+        var password = formData.get("password");
+        var confirmPassword = formData.get("confirmPassword");
+
+        if (!everyoneIsNotEmpty(username, password, confirmPassword)) {
+            showRequiredFieldsError();
+            return;
+        }
+
+        if (!password.equals(confirmPassword)) {
+            showConfirmPasswordError();
+            return;
+        }
+    }
+
+    private void showRequiredFieldsError() {
+        showAlertError("All fields are required");
+    }
+
+    private void showConfirmPasswordError() {
+        showAlertError("Passwords are not identical");
+    }
+
+    private void showAlertError(String contentText) {
+        var errorAlert = new Alert(Alert.AlertType.ERROR);
+        errorAlert.setHeaderText(null);
+        errorAlert.setContentText(contentText);
+        errorAlert.showAndWait();
+    }
+
+    private Boolean everyoneIsNotEmpty(String... strings) {
+        for (String str: strings) {
+            if (str.isEmpty()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
