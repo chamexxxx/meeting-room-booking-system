952fcb059b05ac28d9c773a385bcdf2df7c0ed01
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-18 01:18:19 +0400

    fix router class: make replacing root

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/Router.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/Router.java
@@ -1,26 +1,27 @@
 package com.github.chamexxxx.meetingroombookingsystem;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.HashMap;
 
 public class Router {
-    private static Stage stage;
-    private final static HashMap<String, Scene> sceneMap = new HashMap<>();
+    private static Scene initialScene = null;
+    private final static HashMap<String, Parent> resourceMap = new HashMap<>();
 
-    public static void setStage(Stage stage) {
-        Router.stage = stage;
+    public static void setInitialScene(Scene initialScene) {
+        Router.initialScene = initialScene;
     }
 
     public static void addScene(String sceneName, String resourceName) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource(resourceName));
-        sceneMap.put(sceneName, new Scene(fxmlLoader.load(), 800, 600));
+        resourceMap.put(sceneName, fxmlLoader.load());
     }
 
     public static void switchScene(String sceneName) {
-        stage.setScene(sceneMap.get(sceneName));
+        var root = resourceMap.get(sceneName);
+        initialScene.setRoot(root);
     }
 }
