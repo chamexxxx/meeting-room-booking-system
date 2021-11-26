77f5413c9304ad11d7042b9402f4ef8971e6637e
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-26 15:24:35 +0400

    add after and before actions to router link

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/control/RouterLink.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/control/RouterLink.java
@@ -1,14 +1,36 @@
 package com.github.chamexxxx.meetingroombookingsystem.control;
 
 import com.github.chamexxxx.meetingroombookingsystem.Router;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Hyperlink;
 
 public class RouterLink extends Hyperlink {
     private String to;
+    private EventHandler<ActionEvent> onBeforeAction;
+    private EventHandler<ActionEvent> onAfterAction;
 
     public RouterLink() {
         super();
-        setOnAction(event -> Router.switchScene(to));
+        setOnAction(event -> {
+            if (onBeforeAction != null) {
+                onBeforeAction.handle(event);
+            }
+
+            Router.switchScene(to);
+
+            if (onAfterAction != null) {
+                onAfterAction.handle(event);
+            }
+        });
+    }
+
+    public void setOnBeforeAction(EventHandler<ActionEvent> onBeforeAction) {
+        this.onBeforeAction = onBeforeAction;
+    }
+
+    public void setOnAfterAction(EventHandler<ActionEvent> onAfterAction) {
+        this.onAfterAction = onAfterAction;
     }
 
     public String getTo() {
