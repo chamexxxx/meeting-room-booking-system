ffa4ce5013ae7d8ea0a0fd8ec946b29e642f68dc
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-29 14:42:21 +0400

    change top bar padding

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/control/TopBar.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/control/TopBar.java
@@ -8,6 +8,6 @@ public class TopBar extends VBox {
     public TopBar() {
         getChildren().add(new AccountMenu());
         setAlignment(Pos.CENTER_RIGHT);
-        setPadding(new Insets(30));
+        setPadding(new Insets(30, 10, 30, 10));
     }
 }
