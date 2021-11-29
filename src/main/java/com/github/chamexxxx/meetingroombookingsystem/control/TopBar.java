128ad2ac07cbb872a6ab20623845cc759ba1c78b
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-11-29 19:56:16 +0400

    change top bar styles

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/control/TopBar.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/control/TopBar.java
@@ -6,8 +6,13 @@ import javafx.scene.layout.VBox;
 
 public class TopBar extends VBox {
     public TopBar() {
-        getChildren().add(new AccountMenu());
+        var container = new AccountMenu();
+
+        container.setStyle("-fx-background-color: rgba(255, 255, 255, 0.4); -fx-border-radius: 10px; -fx-background-radius: 10px;");
+        container.setPadding(new Insets(10));
+
         setAlignment(Pos.CENTER_RIGHT);
-        setPadding(new Insets(30, 10, 30, 10));
+        setPadding(new Insets(30, 0, 30, 0));
+        getChildren().add(container);
     }
 }
