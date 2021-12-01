1d4e0415cad8446b3c49cc6f935e92c0ca8cb335
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-12-02 01:38:36 +0400

    refactor top bar

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/control/TopBar.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/control/TopBar.java
@@ -6,13 +6,13 @@ import javafx.scene.layout.VBox;
 
 public class TopBar extends VBox {
     public TopBar() {
-        var container = new AccountMenu();
+        var accountMenu = new AccountMenu();
 
-        container.setStyle("-fx-background-color: rgba(255, 255, 255, 0.4); -fx-border-radius: 10px; -fx-background-radius: 10px;");
-        container.setPadding(new Insets(10));
+        accountMenu.setStyle("-fx-background-color: rgba(255, 255, 255, 0.4); -fx-border-radius: 10px; -fx-background-radius: 10px;");
+        accountMenu.setPadding(new Insets(10));
 
         setAlignment(Pos.CENTER_RIGHT);
         setPadding(new Insets(30, 0, 30, 0));
-        getChildren().add(container);
+        getChildren().add(accountMenu);
     }
 }
