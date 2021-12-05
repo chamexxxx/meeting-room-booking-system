9d51b4cb943e9a52108b3cb06e04720000e8eea6
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-12-05 21:35:02 +0400

    create logo image view

---
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/control/LogoImage.java
@@ -0,0 +1,11 @@
+package com.github.chamexxxx.meetingroombookingsystem.control;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class LogoImage extends ImageView {
+    public LogoImage() {
+        super();
+        setImage(new Image("file:logo.png"));
+    }
+}
