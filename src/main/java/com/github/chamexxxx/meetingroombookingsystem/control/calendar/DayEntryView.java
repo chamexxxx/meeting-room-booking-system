0e48e08680ed43aee295a6c099835ce9dc0b4c35
chamexxxx <59555878+chamexxxx@users.noreply.github.com>
2021-12-05 13:33:59 +0400

    remove unnecessary comments

--- src/main/java/com/github/chamexxxx/meetingroombookingsystem/control/calendar/DayEntryView.java
+++ src/main/java/com/github/chamexxxx/meetingroombookingsystem/control/calendar/DayEntryView.java
@@ -7,11 +7,6 @@ import javafx.scene.control.Skin;
  * Custom {@link com.calendarfx.view.DayEntryView} implementation changing default skin creation
  */
 public class DayEntryView extends com.calendarfx.view.DayEntryView {
-    /**
-     * Constructs a new entry view for the given calendar entry.
-     *
-     * @param entry the entry for which the view will be created
-     */
     public DayEntryView(Entry<?> entry) {
         super(entry);
     }
